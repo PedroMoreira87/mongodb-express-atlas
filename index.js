c1b46918eaa743912f1d8b312badb65d501b4782
Pedro Henrique Moreira <pedrohmoreira87@gmail.com>
2022-10-29 17:09:06 -0300

    fix(example): add cloud mongo db connection

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 // port
 mongoose
   .connect(
-    `mongodb+srv://${user}:${password}@cluster0.trjfjgq.mongodb.net/database?retryWrites=true&w=majority`
+    `mongodb+srv://${user}:${password}@cluster0.p0llsye.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     console.log('Connected to MongoDB!')
